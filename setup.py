c3dcd9ff81bf7b58d6711ab5ae806f44c89b4ebd
Brent Payne <brent.payne@gmail.com>
2014-05-21 22:21:30 -0700

    modifying how the README is loaded for the long_description in setup.py. Attempting to use pypandoc to convert the mardown README to reST format

--- setup.py
+++ setup.py
@@ -2,18 +2,21 @@
 
 from distutils.core import setup
 
-readme = ""
-with open('README.md', 'r') as fp:
-    readme = ''.join(fp)
 
-long_description = 'KennyG create a smooth and easy way to setup and parse' +\
-    'SAX data.' + readme
+def readme():
+    try:
+        import pypandoc
+        description = pypandoc.convert('README.md', 'rst')
+    except (IOError, ImportError, OSError):
+        description = "Could not convert Markdown to reST\n" +\
+            open('README.md').read()
+    return description
 
 setup(
     name='kennyg',
     version='0.1.4',
     description='KennyG SAX Handler',
-    long_description=long_description,
+    long_description=readme(),
     author='Brent Payne',
     author_email='brent.payne@gmail.com',
     url='http://www.github.com/brentpayne/kennyg',
@@ -24,6 +27,7 @@ setup(
     packages=[
         'kennyg',
     ],
+    keywords=['sax', 'xml', 'kenny', 'parser'],
     classifiers=[
         "Topic :: Text Processing :: Markup :: XML",
         "Topic :: Text Processing :: General",
@@ -31,7 +35,8 @@ setup(
         "Programming Language :: Python",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"
+        ("License :: OSI Approved :: GNU Lesser General Public License v3" +
+         " (LGPLv3)")
     ]
 
 )
