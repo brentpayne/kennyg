3005ea6fd9125e5d1045ac1652693b717a95a163
Brent Payne <brent.payne@gmail.com>
2014-05-21 21:46:00 -0700

    updating setup.py with classifications and a longer long description

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@
 
 from distutils.core import setup
 
+readme = ""
+with open('README.md', 'r') as fp:
+    readme = '\n'.join(fp)
+
+long_description = 'KennyG create a smooth and easy way to setup and parse' +\
+    'SAX data.' + readme
+
 setup(
-    name='KennyG',
-    version='0.1.3',
+    name='kennyg',
+    version='0.1.4',
     description='KennyG SAX Handler',
-    long_description=\
-        'KennyG create a smooth and easy way to setup and parse SAX data.',
+    long_description=long_description,
     author='Brent Payne',
     author_email='brent.payne@gmail.com',
     url='http://www.github.com/brentpayne/kennyg',
@@ -17,5 +23,15 @@ setup(
     ],
     packages=[
         'kennyg',
+    ],
+    classifiers=[
+        "Topic :: Text Processing :: Markup :: XML",
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"
     ]
+
 )
